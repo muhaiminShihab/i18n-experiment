b3de6ec639fabc96e49a3a3090eb2c16045c71d5
Muhaimin Shihab <talk.shihab@gmail.com>
2025-08-07 10:47:36 +0600

    feat: enhance UI components for sign-up and login; enhance homepage content to appear more realistic.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignUpComponent},
+  {path: 'sign-up', component: SignUpComponent},
 ];
 
 @NgModule({
